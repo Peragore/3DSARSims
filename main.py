01878c064aaead821382dd07ba1182b22056935a
Philip Mulford (Student) <pmulford@en4146263l.fulton.ad.asu.edu>
2019-11-25 16:11:11 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+xSensors = [[0, 0, 0], [1, 0, 0], [2, 0, 0]]
+ySensors = [[0, 0, 0], [0, 1, 0], [0, 2, 0]]
+zSensors = [[0, 0, 0], [0, 0, 1], [0, 0, 2]]
+
+noiseCoeff = np.normal
+
+
